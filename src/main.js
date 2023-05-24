641ccfa71c161e73091bc32ac0a47a84cbc4884d
Rr210 <1577791638@qq.com>
2023-05-24 23:07:09 +0800

    feat; 初始化;

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
+// 引入Vue
 import Vue from 'vue'
+// 引入App
 import App from './App.vue'
 import router from './router'
-
+// 引入elemetn-ui组件库
+import ElementUI from 'element-ui'
+// 引入element-ui全部css
+import 'element-ui/lib/theme-chalk/index.css'
+// 关闭Vue的生产提示
 Vue.config.productionTip = false
+// 使用element
+Vue.use(ElementUI)
 
 new Vue({
   router,
